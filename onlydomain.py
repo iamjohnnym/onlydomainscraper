e9e4c35aae7ddb00f94b71306e446099f88dbbe3
Ecko <jmartin@ohaiworld.net>
2012-12-07 19:32:03 -0800

    fixed pricing parse

--- onlydomain.py
+++ onlydomain.py
@@ -8,24 +8,24 @@ def get_page(page):
     source = urllib2.urlopen(page)
     return source.read()
 
-def get_next_item(page, start_atr, end_atr, delim):
+def get_next_item(page, start_atr, end_atr, delim, space):
     """
     loops for getting next item
     """
     start = page.find(start_atr)
     if start == -1:
         return None, 0
-    start_period = page.find(delim, start + 1)
+    start_period = page.find(delim, start)
     end_bracket = page.find(end_atr, start_period)
-    item = page[start_period:end_bracket]
+    item = page[start_period + space:end_bracket]
 
     return item, end_bracket
 
-def get_all_items(page, start_atr, end_atr, delim, arr):
+def get_all_items(page, start_atr, end_atr, delim, arr, space):
     x = 0
     #arr = []
     while True:
-        item, end = get_next_item(page, start_atr, end_atr, delim)
+        item, end = get_next_item(page, start_atr, end_atr, delim, space)
         
         if item:
             arr.append(item)
@@ -35,8 +35,8 @@ def get_all_items(page, start_atr, end_atr, delim, arr):
 
 def compile_items(page):
     url = get_page(page)
-    get_all_items(url, "tld['product']", "'", '.', dom)
-    get_all_items(url, "tld['price']", "'", ".", price)
+    get_all_items(url, "tld['product']", "'", ".", dom, 0)
+    get_all_items(url, "tld['price']=", "';", "=", price, 2)
     #for domain, p in (dom, price):
     #    dic[domain] = p
 
